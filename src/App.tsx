93c3dcbd37f78c48a3a38a2474f7c0bc4afa7fb5
madanadra <mlaksindra@gmail.com>
2023-12-07 09:30:25 +0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Box from './components/box'
 import Input from './components/input'
-import { DndContext, closestCenter } from '@dnd-kit/core'
+import { DndContext, DragOverlay, closestCenter } from '@dnd-kit/core'
 import { SortableContext, verticalListSortingStrategy, arrayMove } from '@dnd-kit/sortable'
+import { PiDotsSixVertical } from 'react-icons/pi'
 
 export type Thing = {
   id: string
@@ -12,6 +13,7 @@ export type Thing = {
 function App() {
   const [things, setThings] = useState<Thing[]>([])
   const [text, setText] = useState<string>('')
+  const [active, setActive] = useState<string>('')
 
   const submit = (e: React.FormEvent) => {
     e.preventDefault()
@@ -21,29 +23,41 @@ function App() {
     }
   }
 
-  const handleDragEnd = (e: any) => {
-    const {active, over} = e
+  const handleDragStart = (e: any) => {
+    setActive(e.active.id)
+  }
 
-    if (active.id !== over.id) {
+  const handleDragEnd = (e: any) => {
+    if (active !== e.over.id) {
       setThings(items => {
-        const activeI = items.map(item => item.id).indexOf(active.id)
-        const overI = items.map(item => item.id).indexOf(over.id)
+        const activeI = items.map(item => item.id).indexOf(active)
+        const overI = items.map(item => item.id).indexOf(e.over.id)
 
         return arrayMove(items, activeI, overI)
       })
     }
+
+    setActive('')
   }
 
   return (
-    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+    <DndContext collisionDetection={closestCenter} onDragStart={handleDragStart} onDragEnd={handleDragEnd}>
       <div className='font-main text-slate-50 grid content-start justify-items-center bg-green-500 p-4 min-h-screen'>
         <div className='grid gap-y-2 max-w-sm w-full'>
           <Input submit={submit} text={text} setText={setText} />
           <SortableContext items={things} strategy={verticalListSortingStrategy}>
             {things.length ? 
-            things.map(thing => <Box key={thing.id} thing={thing} />) : 
+            things.map(item => <Box key={item.id} item={item} active={active} />) : 
             <h1 className='font-medium text-green-900 text-center p-4'>Tidak ada data</h1>}
           </SortableContext>
+          <DragOverlay>
+            {active && things.filter(item => item.id === active).map(item => 
+              <div key={item.id} className="flex items-center justify-between gap-x-4 bg-green-700 border border-green-300 rounded-sm p-4 overflow-hidden opacity-30"> 
+                  <h1 className="w-full truncate">{item.name}</h1>
+                  <PiDotsSixVertical className='text-lg cursor-grab active:cursor-grabbing touch-none' />
+              </div>  
+            )}
+          </DragOverlay>
         </div>
       </div>
     </DndContext>
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -3,19 +3,18 @@ import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Thing } from "../App";
 
-export default function Box({thing}: {thing: Thing}) {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({id: thing.id})
+export default function Box({item, active}: {item: Thing, active: string}) {
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({id: item.id})
     const style = {
         transform: CSS.Transform.toString(transform),
-        touchAction: 'none',
         transition
     }
 
     return (
         <div ref={setNodeRef} style={style}  
-        className="flex items-center justify-between gap-x-4 bg-green-700 rounded-sm p-4 overflow-hidden"> 
-            <h1 className="w-full truncate">{thing.name}</h1>
-            <PiDotsSixVertical {...attributes} {...listeners} className='text-lg cursor-grab active:cursor-grabbing' />
+        className={`${item.id === active && 'invisible'} flex items-center justify-between gap-x-4 bg-green-700 rounded-sm p-4 overflow-hidden`}> 
+            <h1 className="w-full truncate">{item.name}</h1>
+            <PiDotsSixVertical {...attributes} {...listeners} className='text-lg cursor-grab active:cursor-grabbing touch-none' />
         </div>
     )
 }
